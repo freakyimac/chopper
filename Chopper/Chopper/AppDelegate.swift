6232f4878de6b9f308bdd6377f4ec003502d8f05
freakyimac <freakyimac@gmail.com>
2023-04-25 01:14:31 +0900

    feat: PlayerViewController 추가 및 디자인 초벌 작업

---
+++ Chopper/Chopper/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Chopper
+//
+//  Created by 김동현 on 2023/04/24.
+//
+
+import UIKit
+import SnapKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = PlayerViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ Chopper/Chopper/PlayerViewController.swift
@@ -0,0 +1,212 @@
+//
+//  PlayerViewController.swift
+//  Chopper
+//
+//  Created by 김동현 on 2023/04/24.
+//
+
+import UIKit
+
+final class PlayerViewController: UIViewController {
+
+    // MARK: - Views
+    private lazy var imageViewRoot: UIView = {
+        let imageView = UIImageView(image: UIImage(named: "blur_test_image"))
+        imageView.contentMode = .scaleAspectFill
+        let blurEffect = UIBlurEffect(style: .dark)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        visualEffectView.frame = self.view.frame
+        imageView.addSubview(visualEffectView)
+        return imageView
+    }()
+    
+    private let imageViewArtwrok: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "blur_test_image"))
+        imageView.backgroundColor = .red
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let labelTitle: UILabel = {
+        let label = UILabel()
+        label.text = "labelTitlelabelTitlelabelTitlelabelTitlelabelTitle"
+        label.font = .boldSystemFont(ofSize: 22)
+        label.textColor = .white
+        return label
+    }()
+    
+    private let labelSubTitle: UILabel = {
+        let label = UILabel()
+        label.text = "labelSubTitlelabelSubTitlelabelSubTitlelabelSubTitlelabelSubTitle"
+        label.font = .systemFont(ofSize: 19)
+        label.textColor = .lightText
+        return label
+    }()
+    
+    private let containerViewSliderProgress: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    private let sliderProgress: UISlider = {
+        let slider = UISlider()
+        return slider
+    }()
+    
+    private let labelCurrentTime: UILabel = {
+        let label = UILabel()
+        label.text = "0:00"
+        label.textColor = .lightText
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let labelTotalTime: UILabel = {
+        let label = UILabel()
+        label.text = "0:00"
+        label.textColor = .lightText
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let containerViewControlButtons: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    private let buttonPlayPause: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "button_play_track"), for: .normal)
+        button.backgroundColor = .systemPink
+        return button
+    }()
+    
+    private let containerViewSliderVolume: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    private let imageViewMinVolume: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .blue
+        return imageView
+    }()
+    
+    private let imageViewMaxVolume: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .blue
+        return imageView
+    }()
+    
+    private let sliderVolume: UISlider = {
+        let slider = UISlider()
+        return slider
+    }()
+    
+    private lazy var stackViewControlPlate: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 0
+        stackView.backgroundColor = .white
+        stackView.distribution = .fill
+        stackView.addArrangedSubview(containerViewSliderProgress)
+        stackView.addArrangedSubview(containerViewControlButtons)
+        stackView.addArrangedSubview(containerViewSliderVolume)
+        return stackView
+    }()
+    
+    // MARK: - Properties
+    private let commonPaddingHorizontal: CGFloat = 15
+    
+    // MARK: - Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    // MARK: - Functions
+    private func setupView() {
+        view.backgroundColor = .white
+        view.addSubview(imageViewRoot)
+        imageViewRoot.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        view.addSubview(imageViewArtwrok)
+        imageViewArtwrok.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(20)
+            make.leading.trailing.equalToSuperview().inset(commonPaddingHorizontal)
+            make.width.height.equalTo(view.frame.width - (commonPaddingHorizontal * 2))
+        }
+        view.addSubview(labelTitle)
+        view.addSubview(labelSubTitle)
+        labelTitle.snp.makeConstraints { make in
+            make.top.equalTo(imageViewArtwrok.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(commonPaddingHorizontal)
+            make.height.equalTo(30)
+        }
+        labelSubTitle.snp.makeConstraints { make in
+            make.top.equalTo(labelTitle.snp.bottom).offset(5)
+            make.leading.trailing.equalToSuperview().inset(commonPaddingHorizontal)
+            make.height.equalTo(30)
+        }
+        containerViewSliderProgress.snp.makeConstraints { make in
+            make.height.equalTo(60)
+        }
+        containerViewSliderProgress.addSubview(sliderProgress)
+        sliderProgress.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+        }
+        containerViewSliderProgress.addSubview(labelCurrentTime)
+        containerViewSliderProgress.addSubview(labelTotalTime)
+        labelCurrentTime.snp.makeConstraints { make in
+            make.top.equalTo(sliderProgress.snp.bottom).offset(5)
+            make.leading.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        labelTotalTime.snp.makeConstraints { make in
+            make.top.equalTo(sliderProgress.snp.bottom).offset(5)
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        containerViewControlButtons.snp.makeConstraints { make in
+            make.height.greaterThanOrEqualTo(50)
+        }
+        containerViewControlButtons.addSubview(buttonPlayPause)
+        buttonPlayPause.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+            make.width.height.equalTo(50)
+        }
+        view.addSubview(stackViewControlPlate)
+        stackViewControlPlate.snp.makeConstraints { make in
+            make.top.equalTo(labelSubTitle.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(commonPaddingHorizontal)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        containerViewSliderVolume.addSubview(sliderVolume)
+        containerViewSliderVolume.addSubview(imageViewMinVolume)
+        containerViewSliderVolume.addSubview(imageViewMaxVolume)
+        imageViewMinVolume.snp.makeConstraints { make in
+            make.width.height.equalTo(20)
+            make.leading.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        imageViewMaxVolume.snp.makeConstraints { make in
+            make.width.height.equalTo(20)
+            make.trailing.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        sliderVolume.snp.makeConstraints { make in
+            make.leading.equalTo(imageViewMinVolume.snp.trailing).offset(10)
+            make.top.bottom.equalToSuperview()
+            make.trailing.equalTo(imageViewMaxVolume.snp.leading).offset(-10)
+        }
+        containerViewSliderVolume.snp.makeConstraints { make in
+            make.height.equalTo(50)
+        }
+    }
+}
